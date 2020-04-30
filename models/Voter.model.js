982ef8ad9cecab947c46c370b8489fa220174e77
wallzack <vulcheek@gmail.com>
2020-04-30 10:02:00 +0200

    25.5 fixed indentation

--- models/Voter.model.js
+++ models/Voter.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const voterSchema = new mongoose.Schema({
-    user: { type: String, required: true },
-    votes: [{ type: mongoose.Schema.Types.ObjectId }],
+  user: { type: String, required: true },
+  votes: [{ type: mongoose.Schema.Types.ObjectId }],
 });
 
 module.exports = mongoose.model('Voter', voterSchema);
